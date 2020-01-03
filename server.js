dc73daffd8c88e74655ffe633df000cc858f5b51
root <root@DESKTOP-FJLQT76.localdomain>
2020-01-03 01:07:15 -0300

    Updated to single quote

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 // Iniciando o App
 const app = express();
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', {
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
-requireDir('./src/models');
+requireDir("./src/models");
 
 // Rotas
-app.use('/api', require('./src/routes'));
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
